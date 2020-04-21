70d2bfeb413cb9c3f6702df3ad62bdd0ea784b16
Ivan Senic <senic.ivan@gmail.com>
2020-04-21 10:46:08 +0200

    updated to the latest formatting

--- components/inspectit-ocelot-configurationserver-ui/src/components/editor/VisualEditor.js
+++ components/inspectit-ocelot-configurationserver-ui/src/components/editor/VisualEditor.js
@@ -5,15 +5,13 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 class VisualEditor extends React.Component {
-
-
   constructor() {
     super();
 
     this.state = {
       isError: false,
-      showWarn: false
-    }
+      showWarn: false,
+    };
   }
 
   componentDidMount() {
@@ -35,20 +33,20 @@ class VisualEditor extends React.Component {
         config = yaml.safeLoad(this.props.yamlConfig);
         normalized = yaml.safeDump(config) === this.props.yamlConfig;
       }
-    
+
       this.setState({
         isError: false,
         config,
-        normalized
-      })
+        normalized,
+      });
     } catch (error) {
       this.setState({
         isError: true,
         config: undefined,
-        normalized: undefined
-      })
+        normalized: undefined,
+      });
     }
-  }
+  };
 
   onUpdate = (config) => {
     const updatedYamlConfig = yaml.safeDump(config);
@@ -59,23 +57,27 @@ class VisualEditor extends React.Component {
     } else {
       this.setState({
         showWarn: true,
-        pendingConfigUpdate: updatedYamlConfig
+        pendingConfigUpdate: updatedYamlConfig,
       });
     }
-  }
+  };
 
   normalizedDoAccept = () => {
     const { pendingConfigUpdate } = this.state;
+    this.setState(
+      {
+        showWarn: false,
+        pendingConfigUpdate: undefined,
+      },
+      () => this.props.onUpdate(pendingConfigUpdate)
+    );
+  };
+
+  normalizedDoDecline = () =>
     this.setState({
       showWarn: false,
-      pendingConfigUpdate: undefined
-    }, () => this.props.onUpdate(pendingConfigUpdate));
-  }
-
-  normalizedDoDecline = () => this.setState({
-    showWarn: false,
-    pendingConfigUpdate: undefined
-  });
+      pendingConfigUpdate: undefined,
+    });
 
   render() {
     const { config, isError, showWarn } = this.state;
@@ -83,20 +85,20 @@ class VisualEditor extends React.Component {
     return (
       <div className="this">
         <style jsx>{`
-                .this {
-                    flex: 1;
-                    display: flex;
-                }
-                .errorBox {
-                    align-self: center;
-                    justify-content: center;
-                    flex: 1;
-                    display: flex;
-                    flex-direction: column;
-                    color: #bbb;
-                    text-align: center;
-                }
-                `}</style>
+          .this {
+            flex: 1;
+            display: flex;
+          }
+          .errorBox {
+            align-self: center;
+            justify-content: center;
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+            color: #bbb;
+            text-align: center;
+          }
+        `}</style>
         <Dialog
           header="Config file overwrite"
           footer={
@@ -111,21 +113,18 @@ class VisualEditor extends React.Component {
           modal={true}
           onHide={() => null}
         >
-          Your configuration file has comments or other non-yaml data. These will be overwritten if you apply the given update. Do you want to continue?
+          Your configuration file has comments or other non-yaml data. These will be overwritten if you apply the given update. Do you want
+          to continue?
         </Dialog>
-        {
-          !isError && this.props.children(this.onUpdate, config)
-        }
-        {
-          isError &&
+        {!isError && this.props.children(this.onUpdate, config)}
+        {isError && (
           <div className="errorBox">
             <p>Properties could not be loaded from the YAML content.</p>
           </div>
-        }
+        )}
       </div>
     );
   }
-
 }
 
 VisualEditor.propTypes = {
@@ -133,9 +132,8 @@ VisualEditor.propTypes = {
   yamlConfig: PropTypes.string,
   /** function to update the yaml config */
   onUpdate: PropTypes.function,
-}
-
-VisualEditor.defaultProps = {
 };
+
+VisualEditor.defaultProps = {};
 
 export default VisualEditor;
